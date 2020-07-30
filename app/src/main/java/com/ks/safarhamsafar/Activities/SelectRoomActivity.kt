0855b707d507959c176fbcad6f5cfe7157f70040
Himaja Penumaka <himaja.mgl2@gmail.com>
2020-07-31 00:45:14 +0530

    safarHamSafar

--- app/src/main/java/com/ks/safarhamsafar/Activities/SelectRoomActivity.kt
+++ app/src/main/java/com/ks/safarhamsafar/Activities/SelectRoomActivity.kt
@@ -2,11 +2,27 @@ package com.ks.safarhamsafar.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import com.ks.safarhamsafar.R
 
 class SelectRoomActivity : AppCompatActivity() {
+
+    lateinit var toolBar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_room)
+
+        toolBar=findViewById(R.id.toolbarSelectRoom)
+        setSupportActionBar(toolBar)
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
 }
